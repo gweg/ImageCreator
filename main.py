cf4098e0754a28b8c3b9515bc7ec3beaa953c72d
goyo <rari@free.fr>
2021-11-26 20:11:59 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import Qt
+import numpy as np
+
+# https://www.programiz.com/python-programming/methods/built-in/property
+import sys
+
+class ImageCreator:
+    def __init__(self):
+        self.matrix=[]
+    def set_matrix(self,matrix_x_size,matrix_y_size):
+        '''
+        square size
+        :param matix_size:
+        :return:
+        '''
+        self.matrix_x_size = matrix_x_size
+        self.matrix_y_size = matrix_y_size
+        self.matrix_size=list(range(0,self.matrix_x_size*self.matrix_y_size))
+
+
+    def paint_matrix(self,painter,x,y,width,height):
+
+        for ry in range(y,y+height,int(height/self.matrix_y_size)+1):
+            for rx in range(x,x+width,int(width/self.matrix_x_size)+1):
+                #painter.setPen(QPen(Qt.green, 1, Qt.SolidLine))
+                painter.setBrush(QBrush(Qt.black, Qt.SolidPattern))
+                painter.drawRect(rx, ry, int(width/self.matrix_x_size), int(height/self.matrix_y_size))
+                print(rx,ry, int(width/self.matrix_x_size), int(height/self.matrix_y_size))
+
+
+
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.imagecreator=ImageCreator()
+        self.imagecreator.set_matrix(64,64)
+
+        self.title = "PyQt5 Drawing Rectangle"
+        self.top = 100
+        self.left = 100
+        self.width = 1280
+        self.height = 768
+        self.resize(1024,768)
+        self.InitWindow()
+
+    def InitWindow(self):
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        #painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
+        # painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+        #painter.setBrush(QBrush(Qt.green, Qt.DiagCrossPattern))
+        self.imagecreator.paint_matrix(painter,100,15,800,600)
+        #painter.drawRect(100, 15, 400, 200)
+
+
+
+App = QApplication(sys.argv)
+
+window = Window()
+sys.exit(App.exec())
